18d4e820f6393d4a9455bc98d68e21afce62110f
Jon Berliner <j.s.berliner@gmail.com>
2018-05-08 13:32:38 -0700

    prep_ims_for_imshow added

--- viz.py
+++ viz.py
@@ -2,6 +2,24 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
+def prep_ims_for_imshow(ims):
+    rank = len(ims.shape)
+    assert rank in [3, 4]
+    if rank == 3:
+        ims = np.expand_dims(ims, 1)
+    assert ims.shape[1] in [1, 3]  # greyscale or rgb
+    bs, ch, height, width = ims.shape
+    nrow = ncol = np.ceil(np.sqrt(bs)).astype(int)
+    ims = np.transpose(ims, [0, 2, 3, 1]).reshape(nrow, ncol, height, width, ch)
+    ims = np.transpose(ims, [0, 2, 1, 3, 4]).reshape(nrow*height, ncol*width, ch)
+    if ch == 1:
+        ims.squeeze(2)
+    return ims
+    
+    
+
+    if len(ims.shape) == 4:
+        assert ims.shape[1] 
 def discrete_cmap(N, base_cmap='Set2'):
     """Create an N-bin discrete colormap from the specified input map"""
 
