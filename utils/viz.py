3706ca597055f3f35a3e76845f8dc0503d328013
Jon Berliner <jsberliner@gmail.com>
2018-08-11 15:20:55 -0400

    viz moved to utils

--- utils/viz.py
+++ utils/viz.py
@@ -10,16 +10,18 @@ def prep_ims_for_imshow(ims):
     assert ims.shape[1] in [1, 3]  # greyscale or rgb
     bs, ch, height, width = ims.shape
     nrow = ncol = np.ceil(np.sqrt(bs)).astype(int)
-    ims = np.transpose(ims, [0, 2, 3, 1]).reshape(nrow, ncol, height, width, ch)
-    ims = np.transpose(ims, [0, 2, 1, 3, 4]).reshape(nrow*height, ncol*width, ch)
+    ims = np.transpose(ims, [0, 2, 3, 1])\
+            .reshape(nrow, ncol, height, width, ch)
+    ims = np.transpose(ims, [0, 2, 1, 3, 4])\
+            .reshape(nrow*height, ncol*width, ch)
     if ch == 1:
         ims.squeeze(2)
     return ims
-    
-    
 
     if len(ims.shape) == 4:
-        assert ims.shape[1] 
+        assert ims.shape[1]
+
+
 def discrete_cmap(N, base_cmap='Set2'):
     """Create an N-bin discrete colormap from the specified input map"""
 
@@ -30,4 +32,3 @@ def discrete_cmap(N, base_cmap='Set2'):
     base = plt.cm.get_cmap(base_cmap)
     cmap = base(np.linspace(0, 1, N))
     return cmap
-
