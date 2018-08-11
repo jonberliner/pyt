273ab29943d3d21974a9bdc2d98c6618a9620dd9
Jon Berliner <jsberliner@gmail.com>
2018-08-11 15:38:20 -0400

    broken; needs to be rewritten for 0.4

--- dropout.py
+++ dropout.py
@@ -4,23 +4,20 @@ from torch.autograd import Variable
 
 # from https://github.com/j-min/Dropouts/blob/master/Gaussian_Variational_Dropout.ipynb
 
+
 class GaussianDropout(nn.Module):
     def __init__(self, alpha=1.0):
         super(GaussianDropout, self).__init__()
-        self.alpha = torch.Tensor([alpha])
-        
+        self.alpha = torch.tensor(alpha)
+
     def forward(self, x):
         """
         Sample noise   e ~ N(1, alpha)
         Multiply noise h = h_ * e
         """
-        if self.train():
+        if self.training:
             # N(1, alpha)
             epsilon = torch.randn(x.size()) * self.alpha + 1
-
-            epsilon = Variable(epsilon)
-            if x.is_cuda:
-                epsilon = epsilon.cuda()
 
             return x * epsilon
         else:
@@ -28,41 +25,42 @@ class GaussianDropout(nn.Module):
 
 
 class VariationalDropout(nn.Module):
-    def __init__(self, alpha=1.0, dim=None):
+    def __init__(self, alpha):
         super(VariationalDropout, self).__init__()
-        
-        self.dim = dim
+
         self.max_alpha = alpha
         # Initial alpha
-        log_alpha = (torch.ones(dim) * alpha).log()
-        self.log_alpha = nn.Parameter(log_alpha)
-        
+        self.log_alpha = nn.Parameter(alpha.log())
+
     def kl(self):
         c1 = 1.16145124
         c2 = -1.50204118
         c3 = 0.58629921
-        
+
         alpha = self.log_alpha.exp()
-        
-        negative_kl = 0.5 * self.log_alpha + c1 * alpha + c2 * alpha**2 + c3 * alpha**3
-        
+
+        negative_kl =\
+            0.5 * self.log_alpha\
+            + c1 * alpha\
+            + c2 * alpha**2\
+            + c3 * alpha**3
+
         kl = -negative_kl
-        
+
         return kl.mean()
-    
+
     def forward(self, x):
         """
         Sample noise   e ~ N(1, alpha)
         Multiply noise h = h_ * e
         """
-        if self.train():
+        if self.training:
             # N(0,1)
-            epsilon = Variable(torch.randn(x.size()))
-            if x.is_cuda:
-                epsilon = epsilon.cuda()
+            epsilon = x.new_zeros(*x.shape).normal_(0., 1.)
 
             # Clip alpha
-            self.log_alpha.data = torch.clamp(self.log_alpha.data, max=self.max_alpha)
+            self.log_alpha.data = torch.clamp(self.log_alpha.data,
+                                              max=self.max_alpha)
             alpha = self.log_alpha.exp()
 
             # N(1, alpha)
